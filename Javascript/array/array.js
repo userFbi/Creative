846ddb184de4c1bb6176cd3940eb9bbc9420b55f
userFbi <pawartushar2024.katargam@gmail.com>
2025-08-22 09:49:20 +0530

    files updated

---
+++ Javascript/array/array.js
@@ -0,0 +1,38 @@
+let data = [10, 20];
+let data1 = [30, 40];
+
+console.log(data);
+
+// at
+// console.log(data.at(5));
+
+// push
+// data.push("tushar");
+// console.log(data);
+
+// pop
+// data.pop();
+// console.log(data);
+
+// fill
+// data.fill(10, 1, 5);
+// console.log(data);
+
+// join
+data.join(data1);
+console.log(data);
+
+// reverse
+// data.reverse();
+// console.log(data);
+
+// shift
+// data.shift();
+// console.log(data);
+
+// unshift
+// data.unshift();
+// console.log(data);
+
+// includes
+// console.log(data.includes(30));
