116240165bec0c650a069613d4639dcc5e9a7eec
userFbi <pawartushar2024.katargam@gmail.com>
2025-08-29 09:49:22 +0530

    files updated

---
+++ Javascript/array/map.js
@@ -0,0 +1,12 @@
+let data = [10, 20, 30, 40, 50];
+console.log(data);
+
+//Map
+let updatedData = data.map((el) => el * 2);
+console.log(updatedData);
+1;
+
+// filter
+let filteredData = data.filter((el) => el < 50);
+console.log(filteredData);
+
