3dc866ca2925e3cec1c2ade54f2d5d90815dfdb8
userFbi <pawartushar2024.katargam@gmail.com>
2025-12-01 09:53:45 +0530

    files updated

---
+++ React/demo-website/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './Hero';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/demo-website/src/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            <img className='hero-img' src="https://www.seriousfitnessboulder.com/uploads/1/3/1/7/131771774/background-images/1805981455.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Hero
