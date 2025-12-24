17517eed731da1f8f4e50e0461a38a260faa9c62
userFbi <pawartushar2024.katargam@gmail.com>
2025-12-24 09:49:38 +0530

    files updated

---
+++ 8.React/6.tasks/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import StudentResult from './Tasks/Student';
+import FilterButtons from './Tasks/Filter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <StudentResult/> */}
+      <FilterButtons />
+    </div>
+  );
+}
+
+export default App;
